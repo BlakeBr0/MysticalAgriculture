1f23699d3e46675cb23ee88200f01d853086b08a
BlakeBr0 <luasmrks@gmail.com>
2017-04-26 16:30:08 -0500

    Release 1.5.2

--- src/main/java/com/blakebr0/mysticalagriculture/items/tools/ItemEssenceSword.java
+++ src/main/java/com/blakebr0/mysticalagriculture/items/tools/ItemEssenceSword.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import com.blakebr0.mysticalagriculture.MysticalAgriculture;
 import com.blakebr0.mysticalagriculture.items.ModItems;
-import com.sun.prism.Material;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
