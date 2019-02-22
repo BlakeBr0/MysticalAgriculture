4adef1d0615c8c5b3a061620c718f45c966824c3
BlakeBr0 <luasmrks@gmail.com>
2019-02-22 00:28:19 -0600

    Put essence recipe configs in their own section in the config gui

--- src/main/java/com/blakebr0/mysticalagriculture/config/ConfigGui.java
+++ src/main/java/com/blakebr0/mysticalagriculture/config/ConfigGui.java
@@ -1,12 +1,13 @@
 package com.blakebr0.mysticalagriculture.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.common.config.ConfigElement;
+import net.minecraftforge.fml.client.config.DummyConfigElement;
 import net.minecraftforge.fml.client.config.GuiConfig;
 import net.minecraftforge.fml.client.config.IConfigElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConfigGui extends GuiConfig {
 
@@ -21,10 +22,18 @@ public class ConfigGui extends GuiConfig {
        		list.add(new ConfigElement(ModConfig.config.getCategory(category)));
        	}
 
+       	List<IConfigElement> essenceRecipeCategories = new ArrayList<>();
        	for(String category : EssenceConfig.config.getCategoryNames()){
-       		list.add(new ConfigElement(EssenceConfig.config.getCategory(category)));
+       		essenceRecipeCategories.add(new ConfigElement(EssenceConfig.config.getCategory(category)));
        	}
-       	
+
+        list.add(new DummyConfigElement.DummyCategoryElement("essence recipes", "essence recipes", essenceRecipeCategories) {
+            @Override
+            public String getComment() {
+                return "Configure the outputs of all essence recipes.";
+            }
+        });
+
         return list;
     }
 }
