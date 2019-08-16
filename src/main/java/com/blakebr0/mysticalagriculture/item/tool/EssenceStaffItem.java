62501cd58e57eb884342ac27d432509fb0d8af0a
BlakeBr0 <luasmrks@gmail.com>
2019-08-15 19:48:37 -0500

    Add essence staffs

---
+++ src/main/java/com/blakebr0/mysticalagriculture/item/tool/EssenceStaffItem.java
@@ -0,0 +1,11 @@
+package com.blakebr0.mysticalagriculture.item.tool;
+
+import com.blakebr0.cucumber.item.BaseItem;
+
+import java.util.function.Function;
+
+public class EssenceStaffItem extends BaseItem {
+    public EssenceStaffItem(Function<Properties, Properties> properties) {
+        super(properties);
+    }
+}
