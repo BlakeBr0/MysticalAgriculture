4c9bc7ad6edc1a197f894309b0748aba487c8a11
BlakeBr0 <luasmrks@gmail.com>
2019-09-25 00:06:44 -0500

    Show name of mod that registered a crop in the seed tooltip if not this

--- src/main/java/com/blakebr0/mysticalagriculture/item/MysticalSeedsItem.java
+++ src/main/java/com/blakebr0/mysticalagriculture/item/MysticalSeedsItem.java
@@ -2,6 +2,7 @@ package com.blakebr0.mysticalagriculture.item;
 
 import com.blakebr0.cucumber.iface.IEnableable;
 import com.blakebr0.cucumber.lib.Localizable;
+import com.blakebr0.mysticalagriculture.MysticalAgriculture;
 import com.blakebr0.mysticalagriculture.api.crop.ICrop;
 import com.blakebr0.mysticalagriculture.api.crop.ICropGetter;
 import com.blakebr0.mysticalagriculture.lib.ModTooltips;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.ModList;
 
 import java.util.List;
 import java.util.function.Function;
@@ -32,6 +34,11 @@ public class MysticalSeedsItem extends BlockNamedItem implements ICropGetter, IE
     public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
         ITextComponent tier = this.crop.getTier().getDisplayName();
         tooltip.add(ModTooltips.CROP_TIER.args(tier).build());
+        if (!this.crop.getModId().equals(MysticalAgriculture.MOD_ID)) {
+            String name = ModList.get().getModFileById(this.crop.getModId()).getMods().get(0).getDisplayName();
+            tooltip.add(ModTooltips.ADDED_BY.args(name).build());
+        }
+
         if (flag.isAdvanced())
             tooltip.add(ModTooltips.CROP_ID.args(this.crop.getId()).color(TextFormatting.DARK_GRAY).build());
     }
--- src/main/java/com/blakebr0/mysticalagriculture/lib/ModTooltips.java
+++ src/main/java/com/blakebr0/mysticalagriculture/lib/ModTooltips.java
@@ -7,6 +7,7 @@ public class ModTooltips {
     public static final Tooltip FILLED = new Tooltip("tooltip.mysticalagriculture.filled");
     public static final Tooltip CROP_TIER = new Tooltip("tooltip.mysticalagriculture.crop_tier");
     public static final Tooltip CROP_ID = new Tooltip("tooltip.mysticalagriculture.crop_id");
+    public static final Tooltip ADDED_BY = new Tooltip("tooltip.mysticalagriculture.added_by");
     public static final Tooltip SECONDARY_CHANCE = new Tooltip("tooltip.mysticalagriculture.secondary_chance");
     public static final Tooltip INFERIUM_OUTPUT = new Tooltip("tooltip.mysticalagriculture.inferium_output");
     public static final Tooltip COOKING_SPEED = new Tooltip("tooltip.mysticalagriculture.cooking_speed");
