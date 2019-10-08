3ed34cd6ea5ec9d804156ffc9084416846b03f79
BlakeBr0 <luasmrks@gmail.com>
2019-10-07 20:14:27 -0500

    Add basic watering can, recipes, and fix ranges

--- src/main/java/com/blakebr0/mysticalagriculture/item/WateringCanItem.java
+++ src/main/java/com/blakebr0/mysticalagriculture/item/WateringCanItem.java
@@ -11,25 +11,19 @@ import net.minecraft.block.FarmlandBlock;
 import net.minecraft.block.IGrowable;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.UseAction;
 import net.minecraft.particles.ParticleTypes;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
-import net.minecraft.util.Hand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IPlantable;
 
@@ -38,16 +32,9 @@ import java.util.Random;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-public class EssenceWateringCanItem extends BaseItem implements IEnableable {
-    private final int range;
-    private final double chance;
-    private final TextFormatting textColor;
-
-    public EssenceWateringCanItem(int range, double chance, TextFormatting textColor, Function<Properties, Properties> properties) {
+public class WateringCanItem extends BaseItem implements IEnableable {
+    public WateringCanItem(Function<Properties, Properties> properties) {
         super(properties.compose(p -> p.maxStackSize(1)));
-        this.range = range;
-        this.chance = chance;
-        this.textColor = textColor;
     }
 
     @Override
@@ -55,42 +42,13 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
         if (this.isEnabled() && this.isInGroup(group)) {
             ItemStack stack = new ItemStack(this);
             NBTHelper.setBoolean(stack, "Water", false);
-            NBTHelper.setBoolean(stack, "Active", false);
             items.add(stack);
-        }
-    }
-
-    @Override
-    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
-        if (selected && world.getGameTime() % 4 == 0) {
-            if (NBTHelper.getBoolean(stack, "Active") && entity instanceof PlayerEntity) {
-                PlayerEntity player = (PlayerEntity) entity;
-                BlockRayTraceResult result = (BlockRayTraceResult) rayTrace(world, player, RayTraceContext.FluidMode.SOURCE_ONLY);
-                if (result.getType() != RayTraceResult.Type.MISS)
-                    this.doWater(stack, world, player, result.getPos(), result.getFace());
-            }
         }
     }
 
     @Override
     public UseAction getUseAction(ItemStack stack) {
         return UseAction.NONE;
-    }
-
-    @Override
-    public boolean hasEffect(ItemStack stack) {
-        return NBTHelper.getBoolean(stack, "Active");
-    }
-
-    @Override
-    public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
-        ItemStack stack = player.getHeldItem(hand);
-
-        if (player.isSneaking() && NBTHelper.getBoolean(stack, "Water")) {
-            NBTHelper.flipBoolean(stack, "Active");
-        }
-
-        return new ActionResult<>(ActionResultType.FAIL, stack);
     }
 
     @Override
@@ -111,9 +69,6 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
             }
         }
 
-        if (NBTHelper.getBoolean(stack, "Active"))
-            return ActionResultType.FAIL;
-
         return this.doWater(stack, world, player, context.getPos(), context.getFace());
     }
 
@@ -124,10 +79,6 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
         } else {
             tooltip.add(ModTooltips.EMPTY.build());
         }
-
-        String rangeString = String.valueOf(this.range);
-        ITextComponent rangeNumber = new StringTextComponent(rangeString + "x" + rangeString).applyTextStyle(this.textColor);
-        tooltip.add(ModTooltips.WATERING_CAN_AREA.args(rangeNumber).build());
     }
 
     @Override
@@ -145,8 +96,7 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
         if (!NBTHelper.getBoolean(stack, "Water"))
             return ActionResultType.FAIL;
 
-        int range = (this.range - 1) / 2;
-        Stream<BlockPos> blocks = BlockPos.getAllInBox(pos.add(-range, -range, -range), pos.add(range, range, range));
+        Stream<BlockPos> blocks = BlockPos.getAllInBox(pos.add(-1, -1, -1), pos.add(1, 1, 1));
         blocks.forEach(aoePos -> {
             BlockState aoeState = world.getBlockState(aoePos);
             if (aoeState.getBlock() instanceof FarmlandBlock) {
@@ -157,11 +107,12 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
             }
         });
 
-        for (int x = -range; x <= range; x++) {
-            for (int z = -range; z <= range; z++) {
-                double d0 = pos.add(x, 0, z).getX() + world.getRandom().nextFloat();
+        Random rand = new Random();
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                double d0 = pos.add(x, 0, z).getX() + rand.nextFloat();
                 double d1 = pos.add(x, 0, z).getY() + 1.0D;
-                double d2 = pos.add(x, 0, z).getZ() + world.getRandom().nextFloat();
+                double d2 = pos.add(x, 0, z).getZ() + rand.nextFloat();
 
                 BlockState state = world.getBlockState(pos);
                 if (state.isSolid() || state.getBlock() instanceof FarmlandBlock) {
@@ -173,8 +124,8 @@ public class EssenceWateringCanItem extends BaseItem implements IEnableable {
         }
 
         if (!world.isRemote()) {
-            if (Math.random() <= this.chance) {
-                blocks = BlockPos.getAllInBox(pos.add(-range, -range, -range), pos.add(range, range, range));
+            if (Math.random() <= 25) {
+                blocks = BlockPos.getAllInBox(pos.add(-3, -3, -3), pos.add(3, 3, 3));
                 blocks.forEach(aoePos -> {
                     Block plantBlock = world.getBlockState(aoePos).getBlock();
                     if (plantBlock instanceof IGrowable || plantBlock instanceof IPlantable || plantBlock == Blocks.MYCELIUM || plantBlock == Blocks.CHORUS_FLOWER) {
