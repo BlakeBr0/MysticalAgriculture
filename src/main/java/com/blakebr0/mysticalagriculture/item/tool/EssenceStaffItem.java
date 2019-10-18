1fa1e32ffe4ff83fb32ea9f4010cc4d503f6aa9f
BlakeBr0 <luasmrks@gmail.com>
2019-10-17 23:55:08 -0500

    Add nyi tooltip to staffs

--- src/main/java/com/blakebr0/mysticalagriculture/item/tool/EssenceStaffItem.java
+++ src/main/java/com/blakebr0/mysticalagriculture/item/tool/EssenceStaffItem.java
@@ -1,11 +1,22 @@
 package com.blakebr0.mysticalagriculture.item.tool;
 
 import com.blakebr0.cucumber.item.BaseItem;
+import com.blakebr0.cucumber.lib.Tooltips;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.function.Function;
 
 public class EssenceStaffItem extends BaseItem {
     public EssenceStaffItem(Function<Properties, Properties> properties) {
         super(properties);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, World world, List<ITextComponent> tooltip, ITooltipFlag flag) {
+        tooltip.add(Tooltips.NOT_YET_IMPLEMENTED.build());
     }
 }
