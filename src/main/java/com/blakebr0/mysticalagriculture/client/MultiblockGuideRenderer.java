365eb3336cfd84a9c1f2da0493a8bd07ee3d76f0
BlakeBr0 <luasmrks@gmail.com>
2020-01-28 21:40:13 -0600

    Failed attempt at re-adding the pedestal guide to the altar

--- src/main/java/com/blakebr0/mysticalagriculture/client/MultiblockGuideRenderer.java
+++ src/main/java/com/blakebr0/mysticalagriculture/client/MultiblockGuideRenderer.java
@@ -2,11 +2,17 @@ package com.blakebr0.mysticalagriculture.client;
 
 import com.blakebr0.mysticalagriculture.block.ModBlocks;
 import com.blakebr0.mysticalagriculture.tileentity.InfusionAltarTileEntity;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderState;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.AtlasTexture;
-import net.minecraft.inventory.container.PlayerContainer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
@@ -15,44 +21,70 @@ import org.lwjgl.opengl.GL14;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
+// TODO: Reimplement ghost pedestals
 public class MultiblockGuideRenderer {
     public static final List<BlockPos> INFUSION_ALTAR_LOCATIONS = new ArrayList<>();
-    
+    public static final RenderType GHOST = RenderType.of(
+            "ghost_gaming_poggers",
+            DefaultVertexFormats.BLOCK,
+            7,
+            2097152,
+            true,
+            true,
+            RenderType.State.builder()
+                    .texture(new RenderState.TextureState(AtlasTexture.LOCATION_BLOCKS_TEXTURE, false, false))
+                    .alpha(new RenderState.AlphaState(0.3F) {
+                        @Override
+                        public void startDrawing() {
+                            RenderSystem.color4f(1F, 1F, 1F, 1F);
+                            RenderSystem.disableLighting();
+                            GlStateManager.enableBlend();
+                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 0.25F);
+                            GlStateManager.blendFunc(GlStateManager.SourceFactor.CONSTANT_ALPHA.field_225655_p_, GlStateManager.DestFactor.ONE_MINUS_CONSTANT_ALPHA.field_225654_o_);
+                        }
+
+                        @Override
+                        public void endDrawing() {
+                            GL14.glBlendColor(1.0F, 1.0F, 1.0F, 1.0F);
+                            GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA.field_225655_p_, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.field_225654_o_);
+                            RenderSystem.enableLighting();
+                        }
+                    })
+                    .build(true));
+
     @SubscribeEvent
     public void onWorldRenderLast(RenderWorldLastEvent event) {
         double posX = Minecraft.getInstance().getRenderManager().info.getProjectedView().getX();
         double posY = Minecraft.getInstance().getRenderManager().info.getProjectedView().getY();
         double posZ = Minecraft.getInstance().getRenderManager().info.getProjectedView().getZ();
 
-        RenderSystem.pushMatrix();
-        RenderSystem.disableLighting();
-        GlStateManager.enableBlend();
-        GL14.glBlendColor(1.0F, 1.0F, 1.0F, 0.25F);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.CONSTANT_ALPHA.field_225655_p_, GlStateManager.DestFactor.ONE_MINUS_CONSTANT_ALPHA.field_225654_o_);
-
+        MatrixStack matrix = event.getMatrixStack();
         Minecraft minecraft = Minecraft.getInstance();
+        ClientWorld world = minecraft.world;
+        if (world == null)
+            return;
+
+        IRenderTypeBuffer.Impl buffers = minecraft.getBufferBuilders().getEntityVertexConsumers();
+        matrix.push();
+        matrix.translate(-posX, -posY, -posZ);
+
         INFUSION_ALTAR_LOCATIONS.forEach(loc -> {
-            TileEntity tile = minecraft.world.getTileEntity(loc);
+            TileEntity tile = world.getTileEntity(loc);
             if (tile instanceof InfusionAltarTileEntity) {
                 InfusionAltarTileEntity altar = (InfusionAltarTileEntity) tile;
                 altar.getPedestalPositions().forEach(pos -> {
-                    if (minecraft.world.isAirBlock(pos)) {
-                        RenderSystem.pushMatrix();
-                        RenderSystem.translated(-posX, -posY, -posZ);
-                        RenderSystem.translated(pos.getX(), pos.getY(), pos.getZ() + 1);
-                        Minecraft.getInstance().getTextureManager().bindTexture(PlayerContainer.BLOCK_ATLAS_TEXTURE); // TODO: IMPLEMENT
-//                        Minecraft.getInstance().getBlockRendererDispatcher().renderBlock(ModBlocks.INFUSION_PEDESTAL.get().getDefaultState(), event.getma);
-                        RenderSystem.color4f(1F, 1F, 1F, 1F);
-                        RenderSystem.popMatrix();
+                    if (world.isAirBlock(pos)) {
+                        matrix.push();
+                        matrix.translate(pos.getX(), pos.getY(), pos.getZ());
+                        IVertexBuilder buffer = buffers.getBuffer(GHOST);
+                        minecraft.getBlockRendererDispatcher().renderBlock(ModBlocks.INFUSION_PEDESTAL.get().getDefaultState(), pos, world, matrix, buffer, false, new Random());
+                        matrix.pop();
                     }
                 });
             }
         });
 
-        GL14.glBlendColor(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA.field_225655_p_, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.field_225654_o_);
-        RenderSystem.enableLighting();
-        RenderSystem.popMatrix();
+        matrix.pop();
     }
 }
