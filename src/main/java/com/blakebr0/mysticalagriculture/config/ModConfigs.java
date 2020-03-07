fe8f3b34959f95b2f15a15374a62307b849e3231
BlakeBr0 <luasmrks@gmail.com>
2020-03-06 23:23:40 -0600

    Added config options for ore/soulstone generation closes https://github.com/BlakeBr0/MysticalAgriculture/issues/279

--- src/main/java/com/blakebr0/mysticalagriculture/config/ModConfigs.java
+++ src/main/java/com/blakebr0/mysticalagriculture/config/ModConfigs.java
@@ -23,6 +23,18 @@ public class ModConfigs {
     public static final ForgeConfigSpec.DoubleValue FERTILIZED_ESSENCE_DROP_CHANCE;
 
     public static final ForgeConfigSpec.DoubleValue SOULIUM_ORE_CHANCE;
+    public static final ForgeConfigSpec.BooleanValue GENERATE_PROSPERITY;
+    public static final ForgeConfigSpec.IntValue PROSPERITY_SPAWN_RATE;
+    public static final ForgeConfigSpec.IntValue PROSPERITY_SPAWN_SIZE;
+    public static final ForgeConfigSpec.IntValue PROSPERITY_SPAWN_HEIGHT;
+    public static final ForgeConfigSpec.BooleanValue GENERATE_INFERIUM;
+    public static final ForgeConfigSpec.IntValue INFERIUM_SPAWN_RATE;
+    public static final ForgeConfigSpec.IntValue INFERIUM_SPAWN_SIZE;
+    public static final ForgeConfigSpec.IntValue INFERIUM_SPAWN_HEIGHT;
+    public static final ForgeConfigSpec.BooleanValue GENERATE_SOULSTONE;
+    public static final ForgeConfigSpec.IntValue SOULSTONE_SPAWN_RATE;
+    public static final ForgeConfigSpec.IntValue SOULSTONE_SPAWN_SIZE;
+    public static final ForgeConfigSpec.IntValue SOULSTONE_SPAWN_HEIGHT;
 
     // Common
     static {
@@ -52,6 +64,54 @@ public class ModConfigs {
                 .comment("The percentage chance a Soulium Ore spawns in a Soulstone cluster.")
                 .translation("configGui.mysticalagriculture.soulium_ore_chance")
                 .defineInRange("souliumOreChance", 0.05, 0, 1);
+        GENERATE_PROSPERITY = common
+                .comment("Should Prosperity Ore generate in the world?")
+                .translation("configGui.mysticalagriculture.generate_prosperity")
+                .define("generateProsperityOre", true);
+        PROSPERITY_SPAWN_RATE = common
+                .comment("How many Prosperity Ore veins should spawn in a chunk.")
+                .translation("configGui.mysticalagriculture.prosperity_spawn_rate")
+                .defineInRange("prosperitySpawnRate", 12, 0, Integer.MAX_VALUE);
+        PROSPERITY_SPAWN_SIZE = common
+                .comment("The maximum amount of Prosperity Ore blocks that can spawn in a vein.")
+                .translation("configGui.mysticalagriculture.prosperity_spawn_size")
+                .defineInRange("prosperitySpawnSize", 8, 0, Integer.MAX_VALUE);
+        PROSPERITY_SPAWN_HEIGHT = common
+                .comment("The maximum Y level that Prosperity Ore veins can spawn.")
+                .translation("configGui.mysticalagriculture.prosperity_spawn_height")
+                .defineInRange("prosperitySpawnHeight", 50, 0, 256);
+        GENERATE_INFERIUM = common
+                .comment("Should Inferium Ore generate in the world?")
+                .translation("configGui.mysticalagriculture.generate_inferium")
+                .define("generateInferiumOre", true);
+        INFERIUM_SPAWN_RATE = common
+                .comment("How many Inferium Ore veins should spawn in a chunk.")
+                .translation("configGui.mysticalagriculture.inferium_spawn_rate")
+                .defineInRange("inferiumSpawnRate", 16, 0, Integer.MAX_VALUE);
+        INFERIUM_SPAWN_SIZE = common
+                .comment("The maximum amount of Inferium Ore blocks that can spawn in a vein.")
+                .translation("configGui.mysticalagriculture.inferium_spawn_size")
+                .defineInRange("inferiumSpawnSize", 8, 0, Integer.MAX_VALUE);
+        INFERIUM_SPAWN_HEIGHT = common
+                .comment("The maximum Y level that Inferium Ore veins can spawn.")
+                .translation("configGui.mysticalagriculture.inferium_spawn_height")
+                .defineInRange("inferiumSpawnHeight", 50, 0, 256);
+        GENERATE_SOULSTONE = common
+                .comment("How many Soulstone veins should spawn in a chunk.")
+                .translation("configGui.mysticalagriculture.generate_soulstone")
+                .define("generateSoulstone", true);
+        SOULSTONE_SPAWN_RATE = common
+                .comment("How many Soulstone veins should spawn in a chunk.")
+                .translation("configGui.mysticalagriculture.soulstone_spawn_rate")
+                .defineInRange("soulstoneSpawnRate", 4, 0, Integer.MAX_VALUE);
+        SOULSTONE_SPAWN_SIZE = common
+                .comment("The maximum amount of Soulstone blocks that can spawn in a vein.")
+                .translation("configGui.mysticalagriculture.soulstone_spawn_size")
+                .defineInRange("soulstoneSpawnSize", 64, 0, Integer.MAX_VALUE);
+        SOULSTONE_SPAWN_HEIGHT = common
+                .comment("The maximum Y level that Soulstone veins can spawn.")
+                .translation("configGui.mysticalagriculture.inferium_spawn_height")
+                .defineInRange("inferiumSpawnHeight", 128, 0, 128);
         common.pop();
 
         COMMON = common.build();
--- src/main/java/com/blakebr0/mysticalagriculture/world/ModWorldFeatures.java
+++ src/main/java/com/blakebr0/mysticalagriculture/world/ModWorldFeatures.java
@@ -1,6 +1,7 @@
 package com.blakebr0.mysticalagriculture.world;
 
 import com.blakebr0.mysticalagriculture.block.ModBlocks;
+import com.blakebr0.mysticalagriculture.config.ModConfigs;
 import com.blakebr0.mysticalagriculture.world.feature.SoulstoneFeature;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
@@ -14,19 +15,34 @@ public class ModWorldFeatures {
     public static void onCommonSetup() {
         ForgeRegistries.BIOMES.forEach(biome -> {
             if (biome.getCategory() == Biome.Category.NETHER) {
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, SoulstoneFeature.INSTANCE.withConfiguration(
-                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, ModBlocks.SOULSTONE.get().getDefaultState(), 64)
-                ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(4, 0, 0, 128))));
+                if (ModConfigs.GENERATE_SOULSTONE.get()) {
+                    int size = ModConfigs.SOULSTONE_SPAWN_SIZE.get();
+                    int rate = ModConfigs.SOULSTONE_SPAWN_RATE.get();
+                    int height = ModConfigs.SOULSTONE_SPAWN_HEIGHT.get();
+                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, SoulstoneFeature.INSTANCE.withConfiguration(
+                            new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NETHERRACK, ModBlocks.SOULSTONE.get().getDefaultState(), size)
+                    ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(rate, 0, 0, height))));
+                }
             } else if (biome.getCategory() == Biome.Category.THEEND) {
                 // the end
             } else {
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.PROSPERITY_ORE.get().getDefaultState(), 8)
-                ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(12, 0, 0, 50))));
+                if (ModConfigs.GENERATE_PROSPERITY.get()) {
+                    int size = ModConfigs.PROSPERITY_SPAWN_SIZE.get();
+                    int rate = ModConfigs.PROSPERITY_SPAWN_RATE.get();
+                    int height = ModConfigs.PROSPERITY_SPAWN_HEIGHT.get();
+                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
+                            new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.PROSPERITY_ORE.get().getDefaultState(), size)
+                    ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(rate, 0, 0, height))));
+                }
 
-                biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
-                        new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.INFERIUM_ORE.get().getDefaultState(), 8)
-                ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(16, 0, 0, 50))));
+                if (ModConfigs.GENERATE_INFERIUM.get()) {
+                    int size = ModConfigs.INFERIUM_SPAWN_SIZE.get();
+                    int rate = ModConfigs.INFERIUM_SPAWN_RATE.get();
+                    int height = ModConfigs.INFERIUM_SPAWN_HEIGHT.get();
+                    biome.addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, Feature.ORE.withConfiguration(
+                            new OreFeatureConfig(OreFeatureConfig.FillerBlockType.NATURAL_STONE, ModBlocks.INFERIUM_ORE.get().getDefaultState(), size)
+                    ).withPlacement(Placement.COUNT_RANGE.configure(new CountRangeConfig(rate, 0, 0, height))));
+                }
             }
         });
     }
