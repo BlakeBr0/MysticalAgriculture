a2a1c11f3aceceb2b5b1fbb912bb178308863918
BlakeBr0 <luasmrks@gmail.com>
2020-03-13 22:37:49 -0500

    Start adding back the old oredict crops

--- src/main/java/com/blakebr0/mysticalagriculture/lib/ModCrops.java
+++ src/main/java/com/blakebr0/mysticalagriculture/lib/ModCrops.java
@@ -17,6 +17,8 @@ import static com.blakebr0.mysticalagriculture.MysticalAgriculture.MOD_ID;
 public class ModCrops {
     private static final CropTextures ELEMENTAL_CROP_TEXTURES = new CropTextures(CropTextures.FLOWER_INGOT_BLANK, CropTextures.ESSENCE_FLAME_BLANK);
     private static final CropTextures ROCK_CROP_TEXTURES = new CropTextures(CropTextures.FLOWER_ROCK_BLANK, CropTextures.ESSENCE_ROCK_BLANK);
+    private static final CropTextures INGOT_CROP_TEXTURES = new CropTextures(CropTextures.FLOWER_INGOT_BLANK, CropTextures.ESSENCE_INGOT_BLANK);
+    private static final CropTextures DUST_CROP_TEXTURES = new CropTextures(CropTextures.FLOWER_DUST_BLANK, CropTextures.ESSENCE_DUST_BLANK);
 
     public static final Crop AIR = new Crop(new ResourceLocation(MOD_ID, "air"), CropTier.ELEMENTAL, CropType.RESOURCE, ELEMENTAL_CROP_TEXTURES, 0xDAD64D, LazyIngredient.item("mysticalagriculture:air_agglomeratio"));
     public static final Crop EARTH = new Crop(new ResourceLocation(MOD_ID, "earth"), CropTier.ELEMENTAL, CropType.RESOURCE, ELEMENTAL_CROP_TEXTURES, 0x54DA4D, LazyIngredient.item("mysticalagriculture:earth_agglomeratio"));
@@ -66,6 +68,33 @@ public class ModCrops {
     public static final Crop DIAMOND = new Crop(new ResourceLocation(MOD_ID, "diamond"), CropTier.FIVE, CropType.RESOURCE, LazyIngredient.tag("forge:gems/diamond"));
     public static final Crop EMERALD = new Crop(new ResourceLocation(MOD_ID, "emerald"), CropTier.FIVE, CropType.RESOURCE, LazyIngredient.tag("forge:gems/emerald"));
     public static final Crop WITHER_SKELETON = new Crop(new ResourceLocation(MOD_ID, "wither_skeleton"), CropTier.FIVE, CropType.MOB, LazyIngredient.item("mysticalagriculture:soul_jar", MobSoulUtils.makeTag(ModMobSoulTypes.WITHER_SKELETON_SOUL_TYPE)));
+
+    // COMMON
+    public static final Crop RUBBER = new Crop(new ResourceLocation(MOD_ID, "rubber"), CropTier.TWO, CropType.RESOURCE, LazyIngredient.EMPTY);
+    public static final Crop SILICON = new Crop(new ResourceLocation(MOD_ID, "silicon"), CropTier.TWO, CropType.RESOURCE, LazyIngredient.EMPTY);
+    public static final Crop SULFUR = new Crop(new ResourceLocation(MOD_ID, "sulfur"), CropTier.TWO, CropType.RESOURCE, DUST_CROP_TEXTURES, 0xFDDC4B, LazyIngredient.EMPTY);
+    public static final Crop ALUMINUM = new Crop(new ResourceLocation(MOD_ID, "aluminum"), CropTier.TWO, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xA4A6B1, LazyIngredient.tag("forge:ingots/aluminum"));
+    public static final Crop COPPER = new Crop(new ResourceLocation(MOD_ID, "copper"), CropTier.TWO, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xF48702, LazyIngredient.tag("forge:ingots/copper"));
+    public static final Crop SALTPETER = new Crop(new ResourceLocation(MOD_ID, "saltpeter"), CropTier.TWO, CropType.RESOURCE, DUST_CROP_TEXTURES, 0xC9BEBD, LazyIngredient.EMPTY);
+    public static final Crop TIN = new Crop(new ResourceLocation(MOD_ID, "tin"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x9ABDD6, LazyIngredient.tag("forge:ingots/tin"));
+    public static final Crop BRONZE = new Crop(new ResourceLocation(MOD_ID, "bronze"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xC98C52, LazyIngredient.tag("forge:ingots/bronze"));
+    public static final Crop ZINC = new Crop(new ResourceLocation(MOD_ID, "zinc"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xE9EBE7, LazyIngredient.tag("forge:ingots/zinc"));
+    public static final Crop BRASS = new Crop(new ResourceLocation(MOD_ID, "brass"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xDAAA4C, LazyIngredient.tag("forge:ingots/brass"));
+    public static final Crop SILVER = new Crop(new ResourceLocation(MOD_ID, "silver"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xA9DBE5, LazyIngredient.tag("forge:ingots/silver"));
+    public static final Crop LEAD = new Crop(new ResourceLocation(MOD_ID, "lead"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x677193, LazyIngredient.tag("forge:ingots/lead"));
+    public static final Crop GRAPHITE = new Crop(new ResourceLocation(MOD_ID, "graphite"), CropTier.THREE, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x545454, LazyIngredient.tag("forge:ingots/graphite"));
+    public static final Crop STEEL = new Crop(new ResourceLocation(MOD_ID, "steel"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x737373, LazyIngredient.tag("forge:ingots/steel"));
+    public static final Crop NICKEL = new Crop(new ResourceLocation(MOD_ID, "nickel"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xD8CC93, LazyIngredient.tag("forge:ingots/nickel"));
+    public static final Crop CONSTANTAN = new Crop(new ResourceLocation(MOD_ID, "constantan"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xD6A04A, LazyIngredient.tag("forge:ingots/constantan"));
+    public static final Crop ELECTRUM = new Crop(new ResourceLocation(MOD_ID, "electrum"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xD5BB4F, LazyIngredient.tag("forge:ingots/electrum"));
+    public static final Crop INVAR = new Crop(new ResourceLocation(MOD_ID, "invar"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xADB7B2, LazyIngredient.tag("forge:ingots/invar"));
+    public static final Crop MITHRIL = new Crop(new ResourceLocation(MOD_ID, "mithril"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x659ABB, LazyIngredient.tag("forge:ingots/mithril"));
+    public static final Crop TUNGSTEN = new Crop(new ResourceLocation(MOD_ID, "tungsten"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x616669, LazyIngredient.tag("forge:ingots/tungsten"));
+    public static final Crop TITANIUM = new Crop(new ResourceLocation(MOD_ID, "titanium"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xD0D1DA, LazyIngredient.tag("forge:ingots/titanium"));
+    public static final Crop URANIUM = new Crop(new ResourceLocation(MOD_ID, "uranium"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x60AE11, LazyIngredient.tag("forge:ingots/uranium"));
+    public static final Crop CHROME = new Crop(new ResourceLocation(MOD_ID, "chrome"), CropTier.FOUR, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xCDB9BD, LazyIngredient.tag("forge:ingots/chrome"));
+    public static final Crop PLATINUM = new Crop(new ResourceLocation(MOD_ID, "platinum"), CropTier.TWO, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0x6FEAEF, LazyIngredient.tag("forge:ingots/platinum"));
+    public static final Crop IRIDIUM = new Crop(new ResourceLocation(MOD_ID, "iridium"), CropTier.TWO, CropType.RESOURCE, INGOT_CROP_TEXTURES, 0xC7C5DC, LazyIngredient.tag("forge:ingots/iridium"));
 
     public static void onRegisterCrops(ICropRegistry registry) {
         INFERIUM.setCrop(ModBlocks.INFERIUM_CROP)
@@ -119,5 +148,32 @@ public class ModCrops {
         registry.register(DIAMOND);
         registry.register(EMERALD);
         registry.register(WITHER_SKELETON);
+
+        // COMMON
+        registry.register(RUBBER);
+        registry.register(SILICON);
+        registry.register(SULFUR);
+        registry.register(ALUMINUM);
+        registry.register(COPPER);
+        registry.register(SALTPETER);
+        registry.register(TIN);
+        registry.register(BRONZE);
+        registry.register(ZINC);
+        registry.register(BRASS);
+        registry.register(SILVER);
+        registry.register(LEAD);
+        registry.register(GRAPHITE);
+        registry.register(STEEL);
+        registry.register(NICKEL);
+        registry.register(CONSTANTAN);
+        registry.register(ELECTRUM);
+        registry.register(INVAR);
+        registry.register(MITHRIL);
+        registry.register(TUNGSTEN);
+        registry.register(TITANIUM);
+        registry.register(URANIUM);
+        registry.register(CHROME);
+        registry.register(PLATINUM);
+        registry.register(IRIDIUM);
     }
 }
