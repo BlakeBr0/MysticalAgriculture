ee1cb24f4cfed9a5bbae7da7d79da57c25c1e57c
BlakeBr0 <luasmrks@gmail.com>
2020-08-31 22:07:30 -0500

    Made cave spiders and zombie villagers give souls

--- src/main/java/com/blakebr0/mysticalagriculture/lib/ModMobSoulTypes.java
+++ src/main/java/com/blakebr0/mysticalagriculture/lib/ModMobSoulTypes.java
@@ -11,6 +11,8 @@ import static com.blakebr0.mysticalagriculture.MysticalAgriculture.MOD_ID;
 
 public final class ModMobSoulTypes {
     private static final Set<ResourceLocation> FISH_IDS = Sets.newHashSet(new ResourceLocation("minecraft:cod"), new ResourceLocation("minecraft:salmon"), new ResourceLocation("minecraft:tropical_fish"), new ResourceLocation("minecraft:pufferfish"));
+    private static final Set<ResourceLocation> ZOMBIE_IDS = Sets.newHashSet(new ResourceLocation("minecraft:zombie"), new ResourceLocation("minecraft:zombie_villager"));
+    private static final Set<ResourceLocation> SPIDER_IDS = Sets.newHashSet(new ResourceLocation("minecraft:spider"), new ResourceLocation("minecraft:cave_spider"));
 
     public static final MobSoulType PIG_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "pig"), new ResourceLocation("minecraft:pig"), 10, 15771042);
     public static final MobSoulType CHICKEN_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "chicken"), new ResourceLocation("minecraft:chicken"), 10, 10592673);
@@ -20,10 +22,10 @@ public final class ModMobSoulTypes {
     public static final MobSoulType FISH_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "fish"), FISH_IDS, "fish", 10, 12691306);
     public static final MobSoulType SLIME_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "slime"), new ResourceLocation("minecraft:slime"), 10, 5349438);
     public static final MobSoulType TURTLE_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "turtle"), new ResourceLocation("minecraft:turtle"), 8, 44975);
-    public static final MobSoulType ZOMBIE_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "zombie"), new ResourceLocation("minecraft:zombie"), 10, 7969893);
+    public static final MobSoulType ZOMBIE_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "zombie"), ZOMBIE_IDS, "zombie", 10, 7969893);
     public static final MobSoulType SKELETON_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "skeleton"), new ResourceLocation("minecraft:skeleton"), 10, 12698049);
     public static final MobSoulType CREEPER_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "creeper"), new ResourceLocation("minecraft:creeper"), 10, 894731);
-    public static final MobSoulType SPIDER_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "spider"), new ResourceLocation("minecraft:spider"), 10, 3419431);
+    public static final MobSoulType SPIDER_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "spider"), SPIDER_IDS, "spider", 10, 3419431);
     public static final MobSoulType RABBIT_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "rabbit"), new ResourceLocation("minecraft:rabbit"), 10, 10051392);
     public static final MobSoulType BLAZE_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "blaze"), new ResourceLocation("minecraft:blaze"), 10, 16167425);
     public static final MobSoulType GHAST_SOUL_TYPE = new MobSoulType(new ResourceLocation(MOD_ID, "ghast"), new ResourceLocation("minecraft:ghast"), 10, 16382457);
