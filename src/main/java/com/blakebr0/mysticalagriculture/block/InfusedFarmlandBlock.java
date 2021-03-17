f4d0c33d96dfcf3a6fe848e2f69d297be7604ebf
BlakeBr0 <luasmrks@gmail.com>
2021-03-16 21:13:08 -0500

    Make essence infused farmland work with silk touch

--- src/main/java/com/blakebr0/mysticalagriculture/block/InfusedFarmlandBlock.java
+++ src/main/java/com/blakebr0/mysticalagriculture/block/InfusedFarmlandBlock.java
@@ -6,8 +6,11 @@ import com.blakebr0.mysticalagriculture.api.farmland.IEssenceFarmland;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FarmlandBlock;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
@@ -34,12 +37,19 @@ public class InfusedFarmlandBlock extends FarmlandBlock implements IColored, IEs
         return type == PlantType.CROP || type == PlantType.PLAINS;
     }
 
+    // TODO: Convert to proper loot table json
     @Override
     public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
         List<ItemStack> drops = new ArrayList<>();
-        drops.add(new ItemStack(Blocks.DIRT)); // TODO: Loot tables_?
-        if (builder.getWorld().getRandom().nextInt(100) < 25)
-            drops.add(new ItemStack(this.tier.getEssence(), 1));
+        ItemStack stack = builder.get(LootParameters.TOOL);
+
+        if (stack != null && EnchantmentHelper.getEnchantmentLevel(Enchantments.SILK_TOUCH, stack) > 0) {
+            drops.add(new ItemStack(this));
+        } else {
+            drops.add(new ItemStack(Blocks.DIRT));
+            if (builder.getWorld().getRandom().nextInt(100) < 25)
+                drops.add(new ItemStack(this.tier.getEssence(), 1));
+        }
 
         return drops;
     }
