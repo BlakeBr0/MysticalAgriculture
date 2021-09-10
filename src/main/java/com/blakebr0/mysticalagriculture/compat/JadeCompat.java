10ae47fdf250294e3af49f63e293484cea44797f
BlakeBr0 <luasmrks@gmail.com>
2021-09-10 17:03:06 -0500

    Update hwyla compat to use jade instead

---
+++ src/main/java/com/blakebr0/mysticalagriculture/compat/JadeCompat.java
@@ -0,0 +1,88 @@
+package com.blakebr0.mysticalagriculture.compat;
+
+import com.blakebr0.cucumber.lib.Colors;
+import com.blakebr0.mysticalagriculture.api.crop.ICropProvider;
+import com.blakebr0.mysticalagriculture.api.farmland.IEssenceFarmland;
+import com.blakebr0.mysticalagriculture.block.EssenceFurnaceBlock;
+import com.blakebr0.mysticalagriculture.block.InferiumCropBlock;
+import com.blakebr0.mysticalagriculture.block.MysticalCropBlock;
+import com.blakebr0.mysticalagriculture.lib.ModTooltips;
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.TooltipPosition;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.world.item.ItemStack;
+
+@WailaPlugin
+public class JadeCompat implements IWailaPlugin {
+    @Override
+    public void register(IRegistrar registrar) {
+        registrar.registerComponentProvider((tooltip, accessor, config) -> {
+            var stack = accessor.getPickedResult();
+            var text = new TextComponent(Colors.WHITE + stack.getHoverName().getString());
+
+            tooltip.add(0, text);
+        }, TooltipPosition.HEAD, MysticalCropBlock.class);
+
+        registrar.registerComponentProvider((tooltip, accessor, config) -> {
+            var block = accessor.getBlock();
+            var crop = ((ICropProvider) block).getCrop();
+
+            tooltip.add(ModTooltips.TIER.args(crop.getTier().getDisplayName()).build());
+
+            var pos = accessor.getPosition();
+            var downPos = pos.below();
+            var level = accessor.getLevel();
+            var belowBlock = level.getBlockState(downPos).getBlock();
+
+            double secondaryChance = crop.getSecondaryChance(belowBlock);
+            if (secondaryChance > 0) {
+                var chanceText = new TextComponent(String.valueOf((int) (secondaryChance * 100)))
+                        .append("%")
+                        .withStyle(crop.getTier().getTextColor());
+
+                tooltip.add(ModTooltips.SECONDARY_CHANCE.args(chanceText).build());
+            }
+
+            var crux = crop.getCruxBlock();
+            if (crux != null) {
+                var stack = new ItemStack(crux);
+                tooltip.add(ModTooltips.REQUIRES_CRUX.args(stack.getHoverName()).build());
+            }
+
+            var biomes = crop.getRequiredBiomes();
+            if (!biomes.isEmpty()) {
+                var biome = level.getBiome(pos);
+                if (!biomes.contains(biome.getRegistryName())) {
+                    tooltip.add(ModTooltips.INVALID_BIOME.color(ChatFormatting.RED).build());
+                }
+            }
+        }, TooltipPosition.BODY, MysticalCropBlock.class);
+
+        registrar.registerComponentProvider((tooltip, accessor, config) -> {
+            var block = accessor.getBlock();
+            var crop = ((ICropProvider) block).getCrop();
+            var downPos = accessor.getPosition().below();
+            var belowBlock = accessor.getLevel().getBlockState(downPos).getBlock();
+
+            int output = 100;
+            if (belowBlock instanceof IEssenceFarmland farmland) {
+                int tier = farmland.getTier().getValue();
+                output = (tier * 50) + 50;
+            }
+
+            var inferiumOutputText = new TextComponent(String.valueOf(output)).append("%").withStyle(crop.getTier().getTextColor());
+
+            tooltip.add(ModTooltips.INFERIUM_OUTPUT.args(inferiumOutputText).build());
+        }, TooltipPosition.BODY, InferiumCropBlock.class);
+
+        registrar.registerComponentProvider((tooltip, accessor, config) -> {
+            var block = accessor.getBlock();
+            var farmland = (IEssenceFarmland) block;
+
+            tooltip.add(ModTooltips.TIER.args(farmland.getTier().getDisplayName()).build());
+        }, TooltipPosition.BODY, EssenceFurnaceBlock.class);
+    }
+}
