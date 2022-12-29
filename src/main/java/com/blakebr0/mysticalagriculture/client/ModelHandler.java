61d3c6a1fb4058c3133c3e9e5441a18f415a0418
BlakeBr0 <luasmrks@gmail.com>
2022-12-29 13:47:17 -0600

    Change model replacement to use json models

--- src/main/java/com/blakebr0/mysticalagriculture/client/ModelHandler.java
+++ src/main/java/com/blakebr0/mysticalagriculture/client/ModelHandler.java
@@ -1,8 +1,7 @@
 package com.blakebr0.mysticalagriculture.client;
 
-import com.blakebr0.cucumber.client.model.RetextureableBlockModelWrapper;
-import com.blakebr0.cucumber.client.model.RetextureableItemModelWrapper;
 import com.blakebr0.mysticalagriculture.MysticalAgriculture;
+import com.blakebr0.mysticalagriculture.api.crop.CropTextures;
 import com.blakebr0.mysticalagriculture.config.ModConfigs;
 import com.blakebr0.mysticalagriculture.init.ModItems;
 import com.blakebr0.mysticalagriculture.item.ExperienceCapsuleItem;
@@ -12,39 +11,29 @@ import com.blakebr0.mysticalagriculture.item.tool.EssenceCrossbowItem;
 import com.blakebr0.mysticalagriculture.item.tool.EssenceFishingRodItem;
 import com.blakebr0.mysticalagriculture.registry.CropRegistry;
 import com.google.common.base.Stopwatch;
-import com.google.common.collect.ImmutableMap;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.renderer.block.model.ItemModelGenerator;
 import net.minecraft.client.renderer.item.ItemProperties;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.model.BakedModel;
-import net.minecraft.client.resources.model.BlockModelRotation;
-import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.ModelEvent;
-import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.client.model.data.ModelData;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public final class ModelHandler {
     private static final ResourceLocation MISSING_NO = new ResourceLocation("minecraft", "missingno");
-    private static final ResourceLocation BLOCK_ATLAS = new ResourceLocation("minecraft", "textures/atlas/blocks.png");
 
     @SubscribeEvent
     public void onRegisterAdditionalModels(ModelEvent.RegisterAdditional event) {
         if (!ModConfigs.ANIMATED_GROWTH_ACCELERATORS.get()) {
-            for (var type : new String[] { "block", "item" }) {
-                for (var tier : new String[] { "inferium", "prudentium", "tertium", "imperium", "supremium" }) {
+            for (var type : List.of("block", "item")) {
+                for (var tier : List.of("inferium", "prudentium", "tertium", "imperium", "supremium")) {
                     event.register(new ResourceLocation(MysticalAgriculture.MOD_ID, String.format("%s/%s_growth_accelerator_static", type, tier)));
                 }
             }
@@ -55,49 +44,50 @@ public final class ModelHandler {
             event.register(new ResourceLocation(MysticalAgriculture.MOD_ID, "block/mystical_mob_crop_" + i));
         }
 
-        event.register(new ResourceLocation(MysticalAgriculture.MOD_ID, "item/mystical_essence"));
-        event.register(new ResourceLocation(MysticalAgriculture.MOD_ID, "item/mystical_seeds"));
+        for (var type : CropRegistry.getInstance().getTypes()) {
+            event.register(new ResourceLocation(CropTextures.FLOWER_INGOT_BLANK + "_" + type.getName()));
+            event.register(new ResourceLocation(CropTextures.FLOWER_ROCK_BLANK + "_" + type.getName()));
+            event.register(new ResourceLocation(CropTextures.FLOWER_DUST_BLANK + "_" + type.getName()));
+            event.register(new ResourceLocation(CropTextures.FLOWER_FACE_BLANK + "_" + type.getName()));
+        }
+
+        event.register(CropTextures.ESSENCE_INGOT_BLANK);
+        event.register(CropTextures.ESSENCE_ROCK_BLANK);
+        event.register(CropTextures.ESSENCE_DUST_BLANK);
+        event.register(CropTextures.ESSENCE_GEM_BLANK);
+        event.register(CropTextures.ESSENCE_TALL_GEM_BLANK);
+        event.register(CropTextures.ESSENCE_DIAMOND_BLANK);
+        event.register(CropTextures.ESSENCE_QUARTZ_BLANK);
+        event.register(CropTextures.ESSENCE_FLAME_BLANK);
+        event.register(CropTextures.ESSENCE_ROD_BLANK);
+
+        event.register(CropTextures.SEED_BLANK);
     }
 
     @SubscribeEvent
-    public void onModelBakingCompleted(ModelEvent.BakingCompleted event) {
+    public void onModifyBakingResults(ModelEvent.ModifyBakingResult event) {
         var stopwatch = Stopwatch.createStarted();
 
         var registry = event.getModels();
-        var bakery = event.getModelBakery();
 
         if (!ModConfigs.ANIMATED_GROWTH_ACCELERATORS.get()) {
-            for (var tier : new String[] { "inferium", "prudentium", "tertium", "imperium", "supremium" }) {
+            for (var tier : List.of("inferium", "prudentium", "tertium", "imperium", "supremium")) {
                 var loc = String.format("%s_growth_accelerator", tier);
                 var blockModel = registry.get(new ResourceLocation(MysticalAgriculture.MOD_ID, "block/" + loc + "_static"));
                 var itemModel = registry.get(new ResourceLocation(MysticalAgriculture.MOD_ID, "item/" + loc + "_static"));
 
-                registry.replace(new ModelResourceLocation(MysticalAgriculture.MOD_ID + ":" + loc), blockModel);
-                registry.replace(new ModelResourceLocation(MysticalAgriculture.MOD_ID + ":" + loc, "inventory"), itemModel);
+                registry.replace(new ModelResourceLocation(MysticalAgriculture.MOD_ID, loc, ""), blockModel);
+                registry.replace(new ModelResourceLocation(MysticalAgriculture.MOD_ID, loc, "inventory"), itemModel);
             }
         }
 
         var cropModels = new HashMap<ResourceLocation, BakedModel[]>();
-        var cropModelsGrown = new HashMap<ResourceLocation, RetextureableBlockModelWrapper>();
 
         for (var cropType : CropRegistry.getInstance().getTypes()) {
             cropModels.put(cropType.getId(), IntStream.range(0, 7)
                     .mapToObj(i -> registry.get(new ResourceLocation(cropType.getStemModel() + "_" + i)))
                     .toArray(BakedModel[]::new));
-
-            var model = bakery.getModel(new ResourceLocation(cropType.getStemModel() + "_7"));
-            var modelWrapper = new RetextureableBlockModelWrapper((BlockModel) model);
-
-            cropModelsGrown.put(cropType.getId(), modelWrapper);
         }
-
-        Function<Material, TextureAtlasSprite> getSprite = bakery.getAtlasSet()::getSprite;
-        var generator = new ItemModelGenerator();
-
-        var essenceModel = bakery.getModel(new ResourceLocation(MysticalAgriculture.MOD_ID, "item/mystical_essence"));
-        var essenceModelWrapper = new RetextureableItemModelWrapper((BlockModel) essenceModel);
-        var seedsModel = bakery.getModel(new ResourceLocation(MysticalAgriculture.MOD_ID, "item/mystical_seeds"));
-        var seedsModelWrapper = new RetextureableItemModelWrapper((BlockModel) seedsModel);
 
         for (var crop : CropRegistry.getInstance().getCrops()) {
             var textures = crop.getTextures();
@@ -109,7 +99,7 @@ public final class ModelHandler {
                     var location = new ModelResourceLocation(cropId, "age=" + i);
                     var bakedModel = registry.get(location);
 
-                    if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).getName().equals(MISSING_NO)) {
+                    if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).contents().name().equals(MISSING_NO)) {
                         var type = crop.getType().getId();
                         registry.replace(location, cropModels.get(type)[i]);
                     }
@@ -118,12 +108,13 @@ public final class ModelHandler {
                 var location = new ModelResourceLocation(cropId, "age=7");
                 var bakedModel = registry.get(location);
 
-                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).getName().equals(MISSING_NO)) {
-                    var texture = crop.getTextures().getFlowerTexture();
-                    var cropRetexturedModel = cropModelsGrown.get(crop.getType().getId()).retexture(ImmutableMap.of("flower", texture.toString()));
-                    var cropBakedModel = cropRetexturedModel.bake(bakery, getSprite, BlockModelRotation.X0_Y0, location);
+                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).contents().name().equals(MISSING_NO)) {
+                    var flower = textures.getFlowerTexture();
+                    var type = crop.getType().getId();
+                    var path = new ResourceLocation(type.getNamespace(), flower.getPath() + "_" + type.getPath());
+                    var model = registry.get(path);
 
-                    registry.replace(location, cropBakedModel);
+                    registry.replace(location, model);
                 }
             }
 
@@ -134,11 +125,9 @@ public final class ModelHandler {
                 var location = new ModelResourceLocation(essenceId, "inventory");
                 var bakedModel = registry.get(location);
 
-                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).getName().equals(MISSING_NO)) {
+                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).contents().name().equals(MISSING_NO)) {
                     var texture = textures.getEssenceTexture();
-                    var retexture = essenceModelWrapper.retexture(ImmutableMap.of("layer0", texture.toString()));
-                    var generated = generator.generateBlockModel(getSprite, retexture);
-                    var model = generated.bake(bakery, generated, getSprite, BlockModelRotation.X0_Y0, location, false);
+                    var model = registry.get(texture);
 
                     registry.replace(location, model);
                 }
@@ -151,11 +140,9 @@ public final class ModelHandler {
                 var location = new ModelResourceLocation(seedsId, "inventory");
                 var bakedModel = registry.get(location);
 
-                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).getName().equals(MISSING_NO)) {
+                if (bakedModel == null || bakedModel.getParticleIcon(ModelData.EMPTY).contents().name().equals(MISSING_NO)) {
                     var texture = textures.getSeedTexture();
-                    var retexture = seedsModelWrapper.retexture(ImmutableMap.of("layer0", texture.toString()));
-                    var generated = generator.generateBlockModel(getSprite, retexture);
-                    var model = generated.bake(bakery, generated, getSprite, BlockModelRotation.X0_Y0, location, false);
+                    var model = registry.get(texture);
 
                     registry.replace(location, model);
                 }
@@ -165,21 +152,6 @@ public final class ModelHandler {
         stopwatch.stop();
 
         MysticalAgriculture.LOGGER.info("Model replacement took {} ms", stopwatch.elapsed(TimeUnit.MILLISECONDS));
-    }
-
-    @SubscribeEvent
-    public void onTextureStitch(TextureStitchEvent event) {
-        if (event.getAtlas().location().equals(BLOCK_ATLAS)) {
-            for (var crop : CropRegistry.getInstance().getCrops()) {
-                var textures = crop.getTextures();
-
-                event.addSprite(textures.getFlowerTexture());
-                event.addSprite(textures.getEssenceTexture());
-                event.addSprite(textures.getSeedTexture());
-            }
-
-            event.addSprite(new ResourceLocation(MysticalAgriculture.MOD_ID, "block/essence_vessel_contents"));
-        }
     }
 
     public static void onClientSetup(FMLClientSetupEvent event) {
