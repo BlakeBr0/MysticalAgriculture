c9863f7e0fa4017e05aa1a4bcab07a84494e00a7
BlakeBr0 <luasmrks@gmail.com>
2023-09-20 21:05:13 -0500

    Use STEP_HEIGHT_ADDITION attribute modifier for step assist Augment Fixes potential mod incompatibilies closes #650

--- src/main/java/com/blakebr0/mysticalagriculture/augment/StepAssistAugment.java
+++ src/main/java/com/blakebr0/mysticalagriculture/augment/StepAssistAugment.java
@@ -1,26 +1,39 @@
 package com.blakebr0.mysticalagriculture.augment;
 
+import com.blakebr0.mysticalagriculture.MysticalAgriculture;
 import com.blakebr0.mysticalagriculture.api.lib.AbilityCache;
 import com.blakebr0.mysticalagriculture.api.tinkering.Augment;
 import com.blakebr0.mysticalagriculture.api.tinkering.AugmentType;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.ForgeMod;
 
 import java.util.EnumSet;
+import java.util.UUID;
 
 public class StepAssistAugment extends Augment {
+    private static final UUID ATTRIBUTE_ID = UUID.fromString("de3d283e-3799-49f5-b1ff-8818b178e057");
+
     public StepAssistAugment(ResourceLocation id, int tier) {
         super(id, tier, EnumSet.of(AugmentType.LEGGINGS, AugmentType.BOOTS), 0xFC4F00, 0x602600);
     }
 
     @Override
-    public void onPlayerTick(Level world, Player player, AbilityCache cache) {
-        if (player.maxUpStep < 1.0625F || !cache.isCached(this, player)) {
-            player.maxUpStep = 1.0625F;
+    public void onPlayerTick(Level level, Player player, AbilityCache cache) {
+        if (!cache.isCached(this, player)) {
+            var height = player.getAttribute(ForgeMod.STEP_HEIGHT_ADDITION.get());
+            if (height == null)
+                return;
+
+            var hasStepAssist = height.getModifier(ATTRIBUTE_ID) != null;
+            if (!hasStepAssist) {
+                height.addPermanentModifier(new AttributeModifier(ATTRIBUTE_ID, MysticalAgriculture.MOD_ID + ":step_assist_augment", 1, AttributeModifier.Operation.ADDITION));
+            }
 
             cache.add(this, player, () -> {
-                player.maxUpStep = 0.6F;
+                height.removeModifier(ATTRIBUTE_ID);
             });
         }
     }
