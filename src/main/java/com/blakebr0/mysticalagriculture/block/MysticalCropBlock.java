ae3742fbee044642026c011f188a447439fbd652
Olfi01 <flom@meyer-alpers.de>
2023-10-29 13:25:54 +0100

    Fix LootContext origin vector rounding

--- src/main/java/com/blakebr0/mysticalagriculture/block/MysticalCropBlock.java
+++ src/main/java/com/blakebr0/mysticalagriculture/block/MysticalCropBlock.java
@@ -74,7 +74,7 @@ public class MysticalCropBlock extends CropBlock implements ICropProvider {
 
             if (vec != null) {
                 var level = builder.getLevel();
-                var pos = new BlockPos((int) vec.x, (int) vec.y, (int) vec.z);
+                var pos = new BlockPos((int) Math.floor(vec.x), (int) Math.floor(vec.y), (int) Math.floor(vec.z));
                 var below = level.getBlockState(pos.below()).getBlock();
                 double chance = this.crop.getSecondaryChance(below);
 
